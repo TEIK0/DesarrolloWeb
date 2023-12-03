234ecf2c98b0a16a5385f6ca654632284a1d0dbb
Teik00 <luisestebanron@gmail.com>
2023-12-03 15:47:11 -0400

    subir pagina

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import logo from './LOGO_UNEG.png'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Universidad Nacional Experimental de Guayana</p>
+        <p>Desarrollo web</p>
+        <p>Luis Ron 30.001.611</p>
+        <p>Profesora Karla Lopez</p>
+        <img src={logo} alt="Logo uneg" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
